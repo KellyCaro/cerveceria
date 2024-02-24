abef321c0d0dd36f1725a614a6b1eddee6bb23c3
kelly.miranda <kelly.miranda@constructorabolivar.com>
2024-02-24 08:20:14 -0500

    conexion con api cervezas

--- cerveceria/src/componentes/ListadoCervezas.jsx
+++ cerveceria/src/componentes/ListadoCervezas.jsx
@@ -1,17 +1,52 @@
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
+
 import { StyleSheet,FlatList, Text, View, TextInput, Button, Alert, StatusBar } from 'react-native';
 import prueba from '../../DatosPrueba/prueba';
 import ItemCerveceria from './ItemCerveceria';
 
 
 const Cervezas = () => {
+  const [cervezas, setCervezas] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchCervezas = async () => {
+    try {
+      const response = await fetch('https://api.openbrewerydb.org/v1/breweries');
+      if (!response.ok) {
+        throw new Error('Error al obtener los datos');
+      }
+      const json = await response.json();
+      if (json && json.length > 0) {
+        setCervezas(json);
+      }
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchCervezas();
+  }, []);
+
+  if (loading) {
+    return <p>Cargando...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+
   return (
   
     
     <FlatList 
     
     style={styles.container}
-    data={prueba}
+    data={cervezas}
     //ItemSeparatorComponent={()=><Text></Text>}
     renderItem={({ item: repo }) => (
         
@@ -20,7 +55,7 @@ const Cervezas = () => {
 >
 
    
-</FlatList>
+    </FlatList>
 
   )
 };
